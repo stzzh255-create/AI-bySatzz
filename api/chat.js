33d9c87fa0c227c14714e4301cb7838a0a49f51b
stzzh255-create <stzzh255@gmail.com>
2026-05-08 19:54:17 +0700

    Add files via upload

---
+++ api/chat.js
@@ -0,0 +1,27 @@
+export default async function handler(req,res){
+
+    const response =
+    await fetch(
+    "https://openrouter.ai/api/v1/chat/completions",
+    {
+        method:"POST",
+
+        headers:{
+            "Authorization":
+            "Bearer " +
+            process.env.OPENROUTER_API_KEY,
+
+            "Content-Type":
+            "application/json"
+        },
+
+        body:JSON.stringify(req.body)
+
+    });
+
+    const data =
+    await response.json();
+
+    res.status(200).json(data);
+
+}
